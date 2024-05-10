a7a9c71ebae16bccd39180b15b2f341f08e4844a
Alexandre Filho <filhoa172@gmail.com>
2024-05-10 17:58:43 -0300

    Menu

--- app/src/main/java/com/example/wizardspotionshop/Menu.java
+++ app/src/main/java/com/example/wizardspotionshop/Menu.java
@@ -1,6 +1,9 @@
 package com.example.wizardspotionshop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends BaseMainActivity {
     @Override
@@ -8,6 +11,33 @@ public class Menu extends BaseMainActivity {
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_menu);
+
+        Button btn_ranking = findViewById(R.id.btn_ranking);
+        Button btn_sair = findViewById(R.id.btn_sair);
+        Button btn_voltar = findViewById(R.id.btn_voltar);
+        btn_ranking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Ranking.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, TelaLogin.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, TelaPrincipal.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
