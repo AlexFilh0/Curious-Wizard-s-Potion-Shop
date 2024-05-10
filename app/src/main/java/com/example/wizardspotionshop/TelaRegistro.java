9f5c6e15c0b4ee172f565425e7e5a69d1aaa4b13
Chewie4458 <marcelavi.sul@gmail.com>
2024-05-10 20:08:43 -0300

    tela_registro1

--- app/src/main/java/com/example/wizardspotionshop/TelaRegistro.java
+++ app/src/main/java/com/example/wizardspotionshop/TelaRegistro.java
@@ -1,6 +1,9 @@
 package com.example.wizardspotionshop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaRegistro extends BaseMainActivity {
     @Override
@@ -8,6 +11,29 @@ public class TelaRegistro extends BaseMainActivity {
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_registro);
+
+        // Define as variáveis dos botões
+        Button btnRegistrar = findViewById(R.id.btn_registrar);
+        Button btnVoltar = findViewById(R.id.btn_voltar);
+
+        // Botão Registar - vai para tela de login
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Adicionar validações de registro
+                Intent intent = new Intent(TelaRegistro.this, TelaLogin.class);
+                startActivity(intent);
+            }
+        });
+
+        // Botão Voltar - vai para tela de login
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TelaRegistro.this, TelaLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
