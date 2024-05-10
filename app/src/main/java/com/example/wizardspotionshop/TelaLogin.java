0fbe2ce0782f9372cd0a72b1efc18b811e126ebf
Marcela <marcelavi.sul@gmail.com>
2024-05-10 18:36:03 -0300

    tela_login1

--- app/src/main/java/com/example/wizardspotionshop/TelaLogin.java
+++ app/src/main/java/com/example/wizardspotionshop/TelaLogin.java
@@ -1,6 +1,9 @@
 package com.example.wizardspotionshop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaLogin extends BaseMainActivity {
     @Override
@@ -8,6 +11,29 @@ public class TelaLogin extends BaseMainActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_login);
+
+        // Define as variáveis dos botões
+        Button btnEntrar = findViewById(R.id.btn_entrar);
+        Button btnRegistrar = findViewById(R.id.btn_registrar);
+
+        // Botão Entrar - vai para tela principal
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 // Adicionar validações de login
+                 Intent intent = new Intent(TelaLogin.this, TelaPrincipal.class);
+                 startActivity(intent);
+             }
+         });
+
+        // Botão Registar - vai para tela principal
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TelaLogin.this, TelaRegistro.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
