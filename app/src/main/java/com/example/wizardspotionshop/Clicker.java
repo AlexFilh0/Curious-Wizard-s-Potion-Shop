a91b381259d725cf8bf52c4d21c30e3e7c38368b
Alexandre <filhoa172@gmail.com>
2024-05-14 20:07:59 -0300

    Tela Final!

--- app/src/main/java/com/example/wizardspotionshop/Clicker.java
+++ app/src/main/java/com/example/wizardspotionshop/Clicker.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.ImageButton;
 import android.widget.TextView;
 
