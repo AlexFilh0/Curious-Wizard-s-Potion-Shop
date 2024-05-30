ef5ce740eb6ad2a05fea85e275b145ac8835e1cf
Marcela <marcelavi.sul@gmail.com>
2024-05-30 16:40:01 -0300

    aaaa

--- app/src/main/java/com/example/wizardspotionshop/Menu.java
+++ app/src/main/java/com/example/wizardspotionshop/Menu.java
@@ -7,12 +7,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.wizardspotionshop.helper.UsuarioDAO;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Menu extends BaseMainActivity {
     private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +40,28 @@ public class Menu extends BaseMainActivity {
         // Define as variáveis dos campos
         TextView txt_welcome = findViewById(R.id.txt_welcome);
 
-        // Busca usuário logado
-        String usuario = usuarioDAO.retornaUsuarioLog();
+        // Define as variáveis do banco ("tabelas")
+        DatabaseReference usuarioBD = referencia.child("usuario");
 
-        // Mostra o nome do usuário
-        txt_welcome.setText("Bem Vindo!");
+        // Busca usuário logado (SQLite)
+        // String usuario = usuarioDAO.retornaUsuarioLog();
+
+        // Recupera o usuário logado
+        DatabaseReference usuarioLogado = usuarioBD.child(auth.getUid());
+        usuarioLogado.child("usuario").addValueEventListener(new ValueEventListener() {
+            String usuario = "";
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usuario = snapshot.getValue().toString();
+                // Mostra o nome do usuário
+                txt_welcome.setText("Bem-vindo, " + usuario.toUpperCase() +"!");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         btn_ranking.setOnClickListener(new View.OnClickListener() {
             @Override
